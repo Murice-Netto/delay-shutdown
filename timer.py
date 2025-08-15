8df2f94fafd3f4a7cca932bc6ff1a1049307fbbd
Mauricio-Nettoo <mauricio.almeida.dev@gmail.com>
2025-08-15 09:57:43 -0300

    feat: adds get_hours_minutes_seconds and its divmod version

--- timer.py
+++ timer.py
@@ -17,3 +17,16 @@ def shutdown() -> None:
             raise RuntimeError(f"OS not supported: {user_os}")
     except Exception as e:
         print(f"Failed to shutdown: {e}")
+
+
+def get_hours_minutes_seconds(total_seconds: int) -> list[int]:
+    hours: int = total_seconds // 3600
+    minutes: int = (total_seconds % 3600) // 60
+    seconds: int = total_seconds % 60
+    return [hours, minutes, seconds]
+
+
+def get_hours_minutes_seconds_divmod(total_seconds: int) -> list[int]:
+    hours, remaining_seconds = divmod(total_seconds, 3600)
+    minutes, seconds = divmod(remaining_seconds, 60)
+    return [hours, minutes, seconds]
